7fe31445bc61e26bdc5be1d2c26bb476e44f45cd
Liz Brown <liz.brown@entercom.com>
2023-01-29 12:47:28 -0700

    project start cleanup & Tailwind add

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import TheWelcome from "./components/TheWelcome.vue";
-</script>
-
 <template>
   <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="./assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <h1 class="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   </header>
 
   <main>
-    <TheWelcome />
+  <h1>main</h1>
   </main>
 </template>
+
+<script setup>
+</script>
 
 <style scoped>
 header {
