384344e912c82dd045dea9853e24c62ae9cd5394
Liz Brown <liz.brown@entercom.com>
2023-02-05 20:51:48 -0700

    new card component

--- src/components/gw2/gw2Tabs/Gw2Card.vue
+++ src/components/gw2/gw2Tabs/Gw2Card.vue
@@ -1,28 +1,28 @@
 <template>
-  <div class="card" :class="item.id">
-    <section v-if="item.id !== 1" class="imgNameQty">
-      <img :src="item.icon" :title="item.name" />
-      {{ item.name }}
+  <div class="card" :class="currency.id">
+    <section v-if="currency.id !== 1" class="imgNameQty">
+      <img :src="currency.icon" :title="currency.name" />
+      <div>{{ currency.name }}</div>
+      <div>{{ currency.amount }}</div>
     </section>
 
-    <section v-if="item.name === 'Coin'" class="imgNameQty coin">
-      {{ item.goldPart }}<img :src="item.goldIcon" title="Gold">
-      {{ item.silverPart }}<img :src="item.silverIcon" title="Silver">
-      {{ item.copperPart }}<img :src="item.copperIcon" title="Copper">
-      <span class="name">{{ item.value }} {{ item.name }}</span>
+    <section v-if="currency.name === 'Coin'" class="imgNameQty coin">
+      <div><img :src="currency.coins.goldIcon" title="Gold">{{ currency.coins.goldCoins }} <span>Gold</span></div>
+      <div><img :src="currency.coins.silverIcon" title="Silver">{{ currency.coins.silverCoins }} <span>Silver</span></div>
+      <div><img :src="currency.coins.copperIcon" title="Copper">{{ currency.coins.copperCoins }}<span>Copper</span></div>     
     </section>
 
     <section class="description">
-      {{ item.description }}
+      {{ currency.description }}
     </section>
   </div>
 </template>
 
 <script setup>
-  const props = defineProps(['item'])
-  const item = props.item
-  const id = item.id
-  console.log('item', item)
+  const props = defineProps(['currency'])
+  const currency = props.currency
+  const id = currency.id
+  // console.log('currency', currency)
 </script>
 
 <style scoped>
@@ -30,7 +30,7 @@
     z-index: 10;
     display: none;
     position: absolute;
-    top: 30px;
+    top: 40px;
     left: 0px;
     right: 0px;
     margin: 0 auto;
@@ -54,10 +54,11 @@
     margin: 0 auto 5px;
   }
 
-  .coin img {
+  .coin > div {
     display: inline-block;
   }
-  
+
+  .coin img,
   .coin span {
     display: block;
   }
