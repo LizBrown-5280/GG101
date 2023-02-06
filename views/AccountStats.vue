35667ce8859b63547e2047805166af7478f5927e
Liz Brown <liz.brown@entercom.com>
2023-02-06 12:40:30 -0700

    import chg update

--- views/AccountStats.vue
+++ views/AccountStats.vue
@@ -22,13 +22,14 @@ import Gw2Header from '@/components/gw2/Gw2Header.vue';
 import Gw2Hero from '@/components/gw2/Gw2Hero.vue'
 import Gw2Key from '@/components/gw2/Gw2Key.vue'
 import Gw2NavTabs from '@/components/gw2/Gw2TabNav.vue'
-import Gw2Account from '@/components/gw2/gw2Tabs/Gw2Account.vue'
-import Gw2Characters from '@/components/gw2/gw2Tabs/Gw2Characters.vue';
-import Gw2Inventories from '@/components/gw2/gw2Tabs/Gw2Inventories.vue';
-import Gw2Progressions from '@/components/gw2/gw2Tabs/Gw2Progressions.vue';
-import Gw2Unlocks from '@/components/gw2/gw2Tabs/Gw2Unlocks.vue';
-import Gw2Wallet from '@/components/gw2/gw2Tabs/Gw2Wallet.vue';
+import Gw2Account from '@/components/gw2/gw2Panels/Gw2Account.vue'
+import Gw2Characters from '@/components/gw2/gw2Panels/Gw2Characters.vue';
+import Gw2Inventories from '@/components/gw2/gw2Panels/Gw2Inventories.vue';
+import Gw2Progressions from '@/components/gw2/gw2Panels/Gw2Progressions.vue';
+import Gw2Unlocks from '@/components/gw2/gw2Panels/Gw2Unlocks.vue';
+import Gw2Wallet from '@/components/gw2/gw2Panels/Gw2Wallet.vue';
 import Gw2Attributes from '../src/components/Gw2Attributes.vue';
+
 </script>
 
 <style scoped>
