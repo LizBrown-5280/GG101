bb7c9c4abd12df37a30d56d7ef26f142732e1774
Liz Brown <liz.brown@entercom.com>
2023-02-15 13:54:18 -0700

    Moved transistion to component

---
+++ src/components/gw2/Gw2Transition.vue
@@ -0,0 +1,24 @@
+<template>
+  <Transition :name="name">
+    <slot></slot>
+  </Transition>
+</template>
+
+<script setup>
+  const props = defineProps(['name'])
+</script>
+
+<style>
+  .slidedown-fade-enter-active {
+    transition: all 0.5s ease-out;
+  }
+
+  .slidedown-fade-leave-active {
+    transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1);
+  }
+
+  .slidedown-fade-enter-from,
+  .slidedown-fade-leave-to {
+    opacity: 0;
+  }
+</style>
