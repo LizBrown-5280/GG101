997e21b9446311e4af59d0075e0249965b780307
Liz Brown <liz.brown@entercom.com>
2023-02-15 13:54:40 -0700

    created restricted component

---
+++ src/components/gw2/gw2Panels/Gw2Restricted.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="restricted">
+    Permissions were not granted to access your {{ type }} with the key provided.
+    <br />
+    To see more, use a Permissions Key that includes access to your{{ type }}.
+  </div>
+</template>
+
+<script setup>
+  const props = defineProps(['type'])
+</script>
