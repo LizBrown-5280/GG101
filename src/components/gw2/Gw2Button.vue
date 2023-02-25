bfdbf242899421340490083a590e12c7469ad6b0
Liz Brown <lizbrown5280@gmail.com>
2023-02-25 12:22:21 -0700

    made btn font smaller & some lint chgs

--- src/components/gw2/Gw2Button.vue
+++ src/components/gw2/Gw2Button.vue
@@ -1,6 +1,5 @@
 <template>
-  <button :data-label="label" type="button" 
-    class="inline-block relative rounded px-2.5 py-1.5">
+  <button :data-label="label" type="button" class="inline-block relative rounded px-2.5 py-1.5">
     <slot />
     <span v-if="label">
       <slot name="label" />
@@ -14,36 +13,38 @@ const label = props.label
 </script>
 
 <style scoped>
-  button {
-    border: 2px solid var(--color-util-primary2);
-    background-color: var(--color-background-primary1);
-    color: var(--color-text-primary3);
-    text-align: left;
-  }
+button {
+  border: 2px solid var(--color-util-primary2);
+  background-color: var(--color-background-primary1);
+  color: var(--color-text-primary3);
+  text-align: left;
+  font-size: 10px;
+}
 
-  .button.label {
-    text-align: center;
-  }
+.button.label {
+  text-align: center;
+}
 
-  button.active, button:hover {
-    border: 2px solid var(--color-util-primary3);
-    background-color: var(--color-background-primary2);
-    color: var(--color-text-primary1);
-    font-weight: bold;
-  }
+button.active,
+button:hover {
+  border: 2px solid var(--color-util-primary3);
+  background-color: var(--color-background-primary2);
+  color: var(--color-text-primary1);
+  font-weight: bold;
+}
 
-  /* Next two are part of what prevents button from shifting right on hover due to font-weight change */
-  button.label::before {
-    content: attr(data-label);
-    font-weight: bold;
-    visibility: hidden;
-  }
+/* Next two are part of what prevents button from shifting right on hover due to font-weight change */
+button.label::before {
+  content: attr(data-label);
+  font-weight: bold;
+  visibility: hidden;
+}
 
-  span {
-    position: absolute;
-    top: var(--vertical-padding);
-    bottom: var(--vertical-padding);
-    left: var(--horizontal-padding);
-    right: var(--horizontal-padding);
-  }
+span {
+  position: absolute;
+  top: var(--vertical-padding);
+  bottom: var(--vertical-padding);
+  left: var(--horizontal-padding);
+  right: var(--horizontal-padding);
+}
 </style>
