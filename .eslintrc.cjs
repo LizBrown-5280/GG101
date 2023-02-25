547413d083cd70e3394194d1826e21512d8da46d
Liz Brown <lizbrown5280@gmail.com>
2023-02-25 12:01:35 -0700

    Owen fixed my linting issues :)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,17 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
+    'prettier',
+    'eslint-config-prettier',
   ],
-  parserOptions: {
-    ecmaVersion: "latest",
+  env: {
+    'vue/setup-compiler-macros': true,
   },
-};
+}
